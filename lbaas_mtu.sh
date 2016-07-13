7f21269447a11c0734b3472afecc0fd0a5926e3d
p.molyavin <p.molyavin@2gis.ru>
2016-07-13 10:33:26 +0600

    Fix messages

--- lbaas_mtu.sh
+++ lbaas_mtu.sh
@@ -5,9 +5,9 @@ do
         if [ `ip netns exec $instance ip link | grep -e 'ns-' | grep -e "mtu 1500" | wc -l` -gt 0 ]
              then
                ip netns exec $instance ip link set mtu $MTU dev `ip netns exec $instance ip link | grep 'ns-' | awk '$2 ~ /ns-/ && /mtu 1500/ { print substr($2, 1, length($2)-1) }'`
-               echo 'Setting MTU 1450 to NS interfaces of lbaas' 
+               echo 'Setting MTU 1450 to discovered lbaas namespaces interfaces' 
         else
-               echo 'NS interfaces are already set to mtu 1450; exiting'       
+               echo 'Lbaas namespace interfaces are already set to mtu $MTU; exiting'       
                exit 0
         fi
 done
